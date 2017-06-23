e6d00052dd72452885be432772319d401c742316
Hadi Yousufi <reach.hadiy@gmail.com>
2017-06-23 17:54:04 -0500

    adds menu model

--- models/menuItem.js
+++ models/menuItem.js
@@ -1,23 +1,37 @@
 //// Import Sequelize library
 module.exports = function (sequelize, DataTypes) {
 
-    // Creates menu model 
-    var menu = sequelize.define("menu", {
-        menu_id: {
+    // Creates item model 
+    var item = sequelize.define("item", {
+        item_id: {
             autoIncrement: true,
             type: DataTypes.INTEGER,
             primaryKey: true
         },
-        menu_name: {
+        item_name: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [1]
             }
         },
-        
-    }, );
+        item_description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        item_category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        item_price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        }
+    }, {
+        timestamps: true,
+        createdAt: true
+    });
 
-    // returns the model we just defined
-    return menu;
+// returns the model we just defined
+return item;
 };
