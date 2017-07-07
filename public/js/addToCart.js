a780e5b6cb57d809be221f753f67682aa6537bc5
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-07 00:42:02 -0500

    Click button leads to session storing and redirecting

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -1,4 +1,23 @@
 // Gets values of all checked items on cart page
-var checkedValues = $('input:checkbox:checked').map(function() {
-    return this.value;
-}).get();
+function getCheckedValues() {
+    var checkedValues = $('input:checkbox:checked').map(function () {
+        return this.value;
+    }).get();
+    return checkedValues;
+}
+
+// Save values of data to sessionStorage
+function storeIdsInSessionStorage() {
+    var menuItemIds = getCheckedValues();
+    sessionStorage.setItem('menuItemIds', menuItemIds);
+}
+
+// redirects to menu page
+function redirectToOrderPage() {
+    window.location = "/order";
+}
+
+$('#add-items-btn').on('click', function(){
+    storeIdsInSessionStorage();
+    redirectToOrderPage();
+});
