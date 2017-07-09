1fc1f4295b8c38246acebd69d3ffa264a55a831d
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-08 23:43:35 -0500

    adds signup.hbs

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var env = require('dotenv').load();
+const exphbs = require('express-handlebars');
 
 var passport = require('passport')
 var session = require('express-session')
 
-
 const app = express();
-const exphbs = require('express-handlebars');
 
 // Middleware
 app.use(express.static(process.cwd() + '/public'));
@@ -16,10 +15,12 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+// Use handlebars as our templating engine
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs'
 }));
+// Set our view engine to be Handlebars i.e. uses .hbs extension on files
 app.set('view engine', '.hbs');
 
 // For Passport
@@ -42,7 +43,7 @@ app.use('/', routes);
 const PORT = process.env.PORT || 3000;
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
-        console.log(`App listening on PORT ${PORT}`);
+        console.log(`Nice! Database looks fine! App listening on PORT ${PORT}`);
     });
 }).catch(function (err) {
     console.log(err);
