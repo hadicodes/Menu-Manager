774d29ea1a7b0c605dbdd9c5521285f6c0d68ee3
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-08 23:22:40 -0500

    adds .env file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+var env = require('dotenv').load();
 
-var passport   = require('passport')
-var session    = require('express-session')
+var passport = require('passport')
+var session = require('express-session')
 
 
 const app = express();
@@ -11,9 +12,24 @@ const exphbs = require('express-handlebars');
 
 // Middleware
 app.use(express.static(process.cwd() + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
+
+// For Passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // Models
 const db = require('./models');
@@ -24,10 +40,10 @@ app.use('/', routes);
 
 // DB connection
 const PORT = process.env.PORT || 3000;
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
         console.log(`App listening on PORT ${PORT}`);
     });
-}).catch(function(err) {
+}).catch(function (err) {
     console.log(err);
 });
