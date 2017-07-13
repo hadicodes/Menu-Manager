95a45e0a5a739137b537b711a7683ddae396eb97
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-12 19:16:18 -0500

    adds orderTotal-api-route

--- routes/orderTotal-api-route.js
+++ routes/orderTotal-api-route.js
@@ -1,11 +1,12 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function (router) {
     // Find all orders and return them to the user with res.json
-    app.get("/api/order", function (req, res) {
+    router.get("/api/order", function (req, res) {
         db.Order.findAll({}).then(function (dbOrder) {
             res.json(dbOrder);
         });
     });
-
 };
+
+module.exports = router;
