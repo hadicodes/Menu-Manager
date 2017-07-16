33e0c8ed3698c2f055f60b16ce1b0f94d6245f18
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-16 15:30:26 -0500

    adds signin form

--- models/user.js
+++ models/user.js
@@ -1,55 +1,24 @@
-module.exports = function(sequelize, Sequelize) {
- 
+module.exports = function (sequelize, Sequelize) {
+
     var User = sequelize.define('user', {
- 
+
         id: {
             autoIncrement: true,
             primaryKey: true,
             type: Sequelize.INTEGER
         },
- 
-        firstName: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        lastName: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
+
         userName: {
             type: Sequelize.TEXT
         },
- 
-        about: {
-            type: Sequelize.TEXT
-        },
- 
-        email: {
-            type: Sequelize.STRING,
-            validate: {
-                isEmail: true
-            }
-        },
- 
+
         password: {
             type: Sequelize.STRING,
             allowNull: false
-        },
- 
-        last_login: {
-            type: Sequelize.DATE
-        },
- 
-        status: {
-            type: Sequelize.ENUM('active', 'inactive'),
-            defaultValue: 'active'
         }
- 
- 
+
     });
- 
+
     return User;
- 
+
 }
